8e205d11db6fa607d8e20189f2d720c86b42f913
BorisSlonov <sslonovborisss@gmail.com>
2024-03-25 20:57:06 +0300

    start final fix

---
+++ php/tg.php
@@ -0,0 +1,51 @@
+<?php
+
+// Замените 'YOUR_BOT_TOKEN' на ваш токен бота от BotFather в Telegram
+$telegramBotToken = '7111598350:AAFveo49waVj7RWvBlsMP00YyxcKIKch3Jk';
+
+// Получаем данные из формы
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
+    $quest = $_POST["quest"];
+
+    // Отправляем данные в Telegram
+    $telegramMessage = "shandra.online:\n\n";
+    $telegramMessage .= "Имя: " . $name . "\n";
+    $telegramMessage .= "Телефон: " . $phone . "\n";
+    if (isset ($quest)) {
+        $telegramMessage .= "Вопрос: " . $quest . "\n";
+    }
+
+    $telegramApiUrl = "https://api.telegram.org/bot" . $telegramBotToken . "/sendMessage";
+
+    // Формируем параметры для POST-запроса
+    $postData = array(
+        'chat_id' => '-4112767528', // Замените на имя вашего канала или группы
+        'text' => $telegramMessage
+    );
+
+
+    // Отправляем POST-запрос в Telegram Bot API с помощью cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $telegramApiUrl);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    // Обрабатываем ответ от Telegram (опционально)
+    // Можно добавить логику для обработки ответа, например, чтобы уведомить пользователя об успешной отправке или об ошибке.
+    $messageSent = false;
+    if ($httpCode == 200) {
+        // The message was successfully sent
+        $messageSent = true;
+        echo 'Спасибо за заявку! Свяжемся с вами в ближайшее время';
+    } else {
+        echo 'Ошибка, попробуйте еще раз';
+    }
+    exit();
+}
+?>
---
+++ src/blocks/modules/personalMentor/personalMentor.js
@@ -0,0 +1,13 @@
+modules.define('personalMentor', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
